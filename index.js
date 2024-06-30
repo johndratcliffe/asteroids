0a81a529ecf32a6983aec4f07902ffa7af0ed50d
John Ratcliffe <2095497@students.wits.ac.za>
2024-06-30 17:53:12 +0200

    Move and rotate player

--- index.js
+++ index.js
@@ -4,16 +4,39 @@ const ctx = canvas.getContext('2d')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-ctx.fillStyle = 'black'
-ctx.fillRect(0, 0, canvas.width, canvas.height)
+const SPEED = 3
+const ROTATIONAL_SPEED = 3
+const FRICTION = 0.99
+
+const keys = {
+  w: {
+    pressed: false
+  },
+  a: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  }
+}
 
 class Player {
   constructor ({ position, velocity }) {
     this.position = position
     this.velocity = velocity
+    this.rotation = 0
   }
 
   draw () {
+    ctx.save()
+    ctx.fillStyle = 'black'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+    ctx.translate(this.position.x, this.position.y)
+    ctx.rotate(this.rotation)
+    ctx.translate(-this.position.x, -this.position.y)
+
+    ctx.beginPath()
     ctx.moveTo(this.position.x + 20, this.position.y)
     ctx.lineTo(this.position.x - 10, this.position.y + 10)
     ctx.lineTo(this.position.x - 10, this.position.y - 10)
@@ -21,6 +44,13 @@ class Player {
 
     ctx.strokeStyle = 'white'
     ctx.stroke()
+    ctx.restore()
+  }
+
+  update () {
+    this.draw()
+    this.position.x += this.velocity.x
+    this.position.y += this.velocity.y
   }
 }
 
@@ -29,4 +59,46 @@ const player = new Player({
   velocity: { x: 0, y: 0 }
 })
 
-player.draw()
+function animate () {
+  window.requestAnimationFrame(animate)
+  player.update()
+  if (keys.w.pressed) {
+    player.velocity.x = Math.cos(player.rotation) * SPEED
+    player.velocity.y = Math.sin(player.rotation) * SPEED
+  } else if (!keys.w.pressed) {
+    player.velocity.x *= FRICTION
+    player.velocity.y *= FRICTION
+  }
+  if (keys.a.pressed) player.rotation -= 0.01 * ROTATIONAL_SPEED
+  else if (keys.d.pressed) player.rotation += 0.01 * ROTATIONAL_SPEED
+}
+
+animate()
+
+window.addEventListener('keydown', (event) => {
+  switch (event.code) {
+    case 'KeyW':
+      keys.w.pressed = true
+      break
+    case 'KeyA':
+      keys.a.pressed = true
+      break
+    case 'KeyD':
+      keys.d.pressed = true
+      break
+  }
+})
+
+window.addEventListener('keyup', (event) => {
+  switch (event.code) {
+    case 'KeyW':
+      keys.w.pressed = false
+      break
+    case 'KeyA':
+      keys.a.pressed = false
+      break
+    case 'KeyD':
+      keys.d.pressed = false
+      break
+  }
+})
